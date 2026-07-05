cf4d1250b02a35685ea98d9d3bc8a444b5832e22
adibah-aslah <adibahaslah.s@gmail.com>
2026-07-05 17:27:08 +0800

    vercel deployment
    Frontend vercel

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import type { RawPollResponse } from "@/types/poll";
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
   headers: { "Content-Type": "application/json" },
 });
 
